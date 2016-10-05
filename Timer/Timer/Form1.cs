0913571ef4a53cb65ec74562f0c22a4903294e17
student <student@STEP.LAN>
2016-10-05 12:05:45 +0300

    ez win form

---
+++ Timer/Timer/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            DisplayTime();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            DisplayTime();
+        }
+        private void DisplayTime()
+        {
+            label1.Text = DateTime.Now.ToLongTimeString();
+
+        }
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class MyWindow : Form
+    {
+        public MyWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Просто текст");
+            MessageBox.Show("Текст с заголовком", "Заголовок");
+            MessageBox.Show("Текст с заголовком и кнопочками", "Заголовок",
+                MessageBoxButtons.AbortRetryIgnore);
+            MessageBox.Show("Текст с заголовком с иконкой Information", "Заголовок",
+             MessageBoxButtons.OK, MessageBoxIcon.Information);
+            MessageBox.Show("Текст с заголовком с иконкой Question", "Заголовок",
+           MessageBoxButtons.OK, MessageBoxIcon.Question);
+            MessageBox.Show("Текст с заголовком с иконкой Asterisk", "Заголовок",
+           MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            MessageBox.Show("Текст с заголовком с иконкой Error", "Заголовок",
+           MessageBoxButtons.OK, MessageBoxIcon.Error);
+           
+
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult res = MessageBox.Show("2 * 2 = 5; (правильное ли тождество)", "Алгебра 1 класс",
+           MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if(res==DialogResult.Yes)
+            {
+                MessageBox.Show("Но но но, сыш, ты че", "Даун",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                MessageBox.Show("Го в Мак бро", "Молодец",
+                  MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+        }
+    }
+}
